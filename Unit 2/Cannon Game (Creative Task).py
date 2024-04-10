c5f6fd8d3e6f2d0e6664d85c4cc1ed4864b8a52f
Jacob <admin@script-ware.net>
2024-04-09 22:33:42 -0400

    Update Cannon Game (Creative Task).py
    Cannonball!!!

--- Unit 2/Cannon Game (Creative Task).py
+++ Unit 2/Cannon Game (Creative Task).py
@@ -5,7 +5,6 @@ app.background = 'skyBlue'
 app.step = 0
 app.startStep = 0
 app.endStep = 0
-# app.stepsPerSecond = 30
 app.slideInc = 6
 app.allowMove = True
 app.powerSlide = False
@@ -15,6 +14,7 @@ app.firingPower = 0
 app.gravity = True
 app.gravityForce = 9.81 # m/s^
 app.singleSplat = 0
+app.splats = []
 
 
 
@@ -44,9 +44,10 @@ powerSlider = Line(5,250,35,250,visible=False)
 # -72.24737361476635
 # 9.19365166790858
 
-def generateSplat(center_x, center_y, radius, num_ovals=6):
-    Circle(center_x, center_y, random.uniform(radius - (radius / 2), radius + radius)) # Draw center of splat
-    
+def generateSplat(center_x:float, center_y:float, radius:float, num_ovals:int=6) -> Group:
+    splat = Group(
+    Circle(center_x, center_y, random.uniform(radius - (radius / 2), radius + radius),fill=None) # Draw center of splat
+    )
     angle_step = 360 / num_ovals
     
     for i in range(num_ovals):
@@ -55,7 +56,7 @@ def generateSplat(center_x, center_y, radius, num_ovals=6):
         
         # Randomize the size of ovals
         oval_width = random.uniform(10, 30)
-        oval_height = random.uniform(30, 40)
+        oval_height = random.uniform(25, 45)
         
         distance = radius + oval_height * 0.01
         oval_x = center_x + distance * math.cos(math.radians(angle))
@@ -65,6 +66,9 @@ def generateSplat(center_x, center_y, radius, num_ovals=6):
         
         # Rotate the oval
         oval.rotateAngle = angle+90
+        splat.add(oval)
+    
+    return splat
 
 def mapToRange(value:float, leftMin:float, leftMax:float, rightMin:float, rightMax:float) -> float:
     leftSpan = leftMax - leftMin
@@ -100,13 +104,11 @@ def onMousePress(mouseX,mouseY):
             cannonball.centerX = cannon.children[2].centerX
             cannonball.centerY = cannon.children[2].centerY
             cannonball.step = 0
-            app.firingPower = mapToRange(abs(powerSlider.y1 - powerBar.bottom), 0, 250, 0, 12)
-            print(f'multiplier: {app.firingPower}')
+            app.firingPower = mapToRange(abs(powerSlider.y1 - powerBar.bottom), 0, 250, 0, 20)
             cannonball.xSpeed = dcos(cannon.rotateAngle) * app.firingPower
             cannonball.ySpeed = dsin(cannon.rotateAngle) * app.firingPower
             app.pew.play(restart=True)
             cannonball.visible = True
-            print(cannonball.xSpeed, cannonball.ySpeed)
             app.singleSplat = False
             
 
@@ -125,10 +127,22 @@ def onStep():
         cannonball.centerY += cannonball.ySpeed
         
         if app.gravity:
-            cannonball.ySpeed += app.gravityForce * (1/30)
+            cannonball.ySpeed += app.gravityForce * (1/app.stepsPerSecond)
         
-        if cannonball.right >= 400 or cannonball.bottom >= 400:
+        if cannonball.right >= 400 or cannonball.bottom >= 400 or cannonball.top <= 0:
             if not app.singleSplat:
+                paint_splat = generateSplat(200, 200, random.uniform(11,16), num_ovals=int(random.uniform(6,8)))
+                paint_splat.centerX = cannonball.centerX
+                paint_splat.centerY = cannonball.centerY
+                if paint_splat.bottom > 400:
+                    paint_splat.bottom = 400
+                if paint_splat.right > 400:
+                    paint_splat.right = 400
+                if paint_splat.top < 0:
+                    paint_splat.top = 0
+                for s in [i for i in app.splats if i.opacity > 0]:
+                        s.opacity -= 25
+                app.splats.append(paint_splat)
                 app.splat.play(restart=True)
                 app.singleSplat = True
         
@@ -138,5 +152,3 @@ def onStep():
             powerSlider.visible = False
             app.allowMove = True
 
-
-generateSplat(200, 200, 11, num_ovals=6)
