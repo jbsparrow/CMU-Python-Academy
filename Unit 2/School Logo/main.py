6e1f9c4cfb6d72c6e5b70479358880e3c240145e
Jacob <admin@script-ware.net>
2024-04-15 10:16:27 -0400

    Simplify Code
    Removed the mirror function and switched to using a class for generating beziers instead of a bunch of functions.

--- Unit 2/School Logo/main.py
+++ Unit 2/School Logo/main.py
@@ -5,8 +5,54 @@ jag.centerX = 200
 jag.centerY = 200
 
 
-class app():
-    pass
+class Bezier:
+    def __init__(self, control_points, num_segments):
+        """Initialize the Bezier curve with control points and a specific number of segments."""
+        self.control_points = control_points
+        self.num_segments = num_segments
+        self.group = self.create_bezier_group()
+
+    def cubic_bezier(self, t, P0, P1, P2, P3):
+        """Calculate the point on the cubic Bezier curve for a given parameter t."""
+        x = (1 - t)**3 * P0[0] + 3 * (1 - t)**2 * t * P1[0] + 3 * (1 - t) * t**2 * P2[0] + t**3 * P3[0]
+        y = (1 - t)**3 * P0[1] + 3 * (1 - t)**2 * t * P1[1] + 3 * (1 - t) * t**2 * P2[1] + t**3 * P3[1]
+        return (x, y)
+
+    def create_bezier_group(self):
+        """Create a group of line segments to represent the Bezier curve."""
+        P0, P1, P2, P3 = self.control_points
+        points = [self.cubic_bezier(t / self.num_segments, P0, P1, P2, P3) for t in range(self.num_segments + 1)]
+        group = Group()
+        for i in range(len(points) - 1):
+            line = Line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
+            group.add(line)
+        return group
+
+    def update(self, control_points=None, num_segments=None):
+        """Update the Bezier curve with new control points or a different number of segments."""
+        if control_points:
+            self.control_points = control_points
+        if num_segments is not None:
+            self.num_segments = num_segments
+
+        points = [self.cubic_bezier(t / self.num_segments, *self.control_points) for t in range(self.num_segments + 1)]
+        current_lines = len(self.group.children)
+
+        # Update existing lines or add new ones if necessary
+        for i in range(max(current_lines, self.num_segments)):
+            if i < current_lines and i < self.num_segments:
+                line = self.group.children[i]
+                line.x1 = points[i][0]
+                line.y1 = points[i][1]
+                line.x2 = points[i + 1][0]
+                line.y2 = points[i + 1][1]
+            elif i >= current_lines and i < self.num_segments:
+                new_line = Line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
+                self.group.add(new_line)
+
+        # Remove excess lines if the number of segments decreased
+        while len(self.group.children) > self.num_segments:
+            self.group.remove(self.group.children[-1])
 
 
 
@@ -27,180 +73,28 @@ app.p1Label = Label('2', app.p1[0], app.p1[1], size=12, fill='crimson')
 app.p2Label = Label('3', app.p2[0], app.p2[1], size=12, fill='crimson')
 app.p3Label = Label('4', app.p3[0], app.p3[1], size=12, fill='crimson')
 
-def cubic_bezier(t: int, P0: tuple, P1: tuple, P2: tuple, P3: tuple) -> tuple:
-    """Calculate the point on the cubic Bezier curve for current time(t)."""
-    x = (1 - t)**3 * P0[0] + 3 * (1 - t)**2 * t * P1[0] + 3 * (1 - t) * t**2 * P2[0] + t**3 * P3[0]
-    y = (1 - t)**3 * P0[1] + 3 * (1 - t)**2 * t * P1[1] + 3 * (1 - t) * t**2 * P2[1] + t**3 * P3[1]
-    return (x, y)
-
-def draw_bezier(control_points: list[tuple], num_segments: int) -> Polygon:
-    """Draws a bezier curve with the specified control points and segments."""
-    P0, P1, P2, P3 = control_points
-    points = [cubic_bezier(t / num_segments, P0, P1, P2, P3) for t in range(num_segments + 1)]
-    shape = Polygon(0,0,1,1,fill=rgb(67,67,67),border='black',opacity=60)
-    pointList = [[x, y] for x, y in points]
-    setattr(shape, 'pointList', pointList)
-    return shape
-
-app.bezier = draw_bezier([app.p0,app.p1,app.p2,app.p3],15)
-
-def draw_bezier_sketch(control_points: list[tuple], num_segments:int) -> None:
-    """Draws a bezier curve with the specified control points and segments that is to be used for designing the drawing. Points can be exported by pressing return."""
-    P0, P1, P2, P3 = control_points
-    points = [cubic_bezier(t / num_segments, P0, P1, P2, P3) for t in range(num_segments + 1)]
-    pointList = [[x, y] for x, y in points]
-    setattr(app.bezier, 'pointList', pointList)
-    return None
-
-def create_bezier_group(control_points, num_segments):
-    """Create a group of line segments to represent the Bezier curve."""
-    P0, P1, P2, P3 = control_points
-    points = [cubic_bezier(t / num_segments, P0, P1, P2, P3) for t in range(num_segments + 1)]
-    bezier_group = Group()
-    for i in range(len(points) - 1):
-        line = Line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
-        bezier_group.add(line)
-    return bezier_group
-
-def update_bezier_group(bezier_group, control_points, num_segments):
-    """Efficiently update the group of line segments for the Bezier curve."""
-    P0, P1, P2, P3 = control_points
-    points = [cubic_bezier(t / num_segments, P0, P1, P2, P3) for t in range(num_segments + 1)]
-    current_lines = len(bezier_group.children)
-    
-    # Update existing lines or add new ones if necessary
-    for i in range(max(current_lines, num_segments)):
-        if i < current_lines and i < num_segments:
-            line = bezier_group.children[i]
-            line.x1 = points[i][0]
-            line.y1 = points[i][1]
-            line.x2 = points[i + 1][0]
-            line.y2 = points[i + 1][1]
-        elif i >= current_lines and i < num_segments:
-            new_line = Line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
-            bezier_group.add(new_line)
-
-    # Remove excess lines if the number of segments decreased
-    while len(bezier_group.children) > num_segments:
-        bezier_group.remove(bezier_group.children[-1])
+# app.bezier = Bezier([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
 
 
-app.bezierLine = create_bezier_group([app.p0,app.p1,app.p2,app.p3],15)
-app.bezierLines = Group()
+app.bezierLine = Bezier([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
+app.bezierLines = []
 app.selectingNewStart = False
 app.selectedStart = 0
 app.lockedPoint = 0
 
-class mirror():
-    def __init__(self, shape):
-        self.shape = shape
-        self.mirroredShape = self.mirrorShape()
-        return self.mirroredShape
-
-    def insertAttributes(self, shape, dict: dict):
-        for key, value in dict.items():
-            try:
-                setattr(shape, key, value)
-            except:
-                pass
-        return shape
-
-
-    def mirrorRect(self, rect):
-        shapeAttributes = {'fill': rect.fill, 'border': rect.border, 'borderWidth': rect.borderWidth, 'opacity': rect.opacity, 'dashes': rect.dashes, 'centerX': 400 - rect.centerX, 'centerY': rect.centerY}
-        newShape = Line(0, 0, rect.width, rect.height)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorOval(self, oval):
-        shapeAttributes = {'fill': oval.fill, 'border': oval.border, 'borderWidth': oval.borderWidth, 'opacity': oval.opacity, 'dashes': oval.dashes, 'centerX': 400 - oval.centerX, 'centerY': oval.centerY}
-        newShape = Oval(0, 0, oval.width, oval.height)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorCircle(self, circle):
-        shapeAttributes = {'fill': circle.fill, 'border': circle.border, 'borderWidth': circle.borderWidth, 'opacity': circle.opacity, 'dashes': circle.dashes, 'centerX': 400 - circle.centerX, 'centerY': circle.centerY}
-        newShape = Circle(0, 0, circle.radius)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorLine(self, line):
-        shapeAttributes = {'arrowStart': line.arrowEnd, 'arrowEnd': line.arrowStart, 'fill': line.fill, 'border': line.border, 'borderWidth': line.borderWidth, 'opacity': line.opacity, 'dashes': line.dashes}
-        newShape = Line(400 - line.x1, line.y1, 400 - line.x2, line.y2)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorLabel(self, label):
-        shapeAttributes = {'size': label.size, 'font': label.font, 'bold': label.bold, 'italic': label.italic, 'fill': label.fill, 'border': label.border, 'borderWidth': label.borderWidth, 'opacity': label.opacity, 'dashes': label.dashes, 'centerX': 400 - label.centerX, 'centerY': label.centerY}
-        newShape = Label(label.value, 0, 0)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorRegularPolygon(self, regularPolygon):
-        shapeAttributes = {'fill': regularPolygon.fill, 'border': regularPolygon.border, 'borderWidth': regularPolygon.borderWidth, 'opacity': regularPolygon.opacity, 'dashes': regularPolygon.dashes, 'centerX': 400 - regularPolygon.centerX, 'centerY': regularPolygon.centerY}
-        newShape = RegularPolygon(0, 0, regularPolygon.radius, regularPolygon.points)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorStar(self, star):
-        shapeAttributes = {'fill': star.fill, 'border': star.border, 'borderWidth': star.borderWidth, 'roundness': star.roundness, 'opacity': star.opacity, 'dashes': star.dashes, 'centerX': 400 - star.centerX, 'centerY': star.centerY}
-        newShape = Star(0, 0, star.radius, star.points)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorPolygon(self, polygon):
-        shapeAttributes = {'fill': polygon.fill, 'border': polygon.border, 'borderWidth': polygon.borderWidth, 'opacity': polygon.opacity, 'dashes': polygon.dashes}
-        points = polygon.pointList # structure: [[x1, y1], [x2, y2], ...]
-        newPoints = []
-        for point in points:
-            newPoints.append(400 - point[0])
-            newPoints.append(point[1])
-            # newPoints.append(400 - point[0])
-        newShape = Polygon(*newPoints)
-        # print(newPoints[::-1])
-        return self.insertAttributes(newShape, shapeAttributes)
-
-    def mirrorArc(self, arc):
-        shapeAttributes = {'fill': arc.fill, 'border': arc.border, 'borderWidth': arc.borderWidth, 'opacity': arc.opacity, 'dashes': arc.dashes, 'centerX': 400 - arc.centerX, 'centerY': arc.centerY, 'width': arc.width, 'height': arc.height}
-        newShape = Arc(0, 0, arc.width*2, arc.height*2, arc.startAngle+180, arc.sweepAngle)
-        return self.insertAttributes(newShape, shapeAttributes)
-
-
-    def mirrorShape(self, shape=None):
-        if shape != None:
-            self.shape = shape
-        if isinstance(self.shape, Group):
-            mirroredShapes = Group()
-            for child in self.shape.children:
-                mirroredShapes.add(self.mirrorShape(child))
-            return mirroredShapes
-        elif isinstance(self.shape, Rect):
-            return self.mirrorRect(self.shape)
-        elif isinstance(self.shape, Oval):
-            return self.mirrorOval(self.shape)
-        elif isinstance(self.shape, Circle):
-            return self.mirrorCircle(self.shape)
-        elif isinstance(self.shape, Line):
-            return self.mirrorLine(self.shape)
-        elif isinstance(self.shape, Label):
-            return self.mirrorLabel(self.shape)
-        elif isinstance(self.shape, RegularPolygon):
-            return self.mirrorRegularPolygon(self.shape)
-        elif isinstance(self.shape, Star):
-            return self.mirrorStar(self.shape)
-        elif isinstance(self.shape, Polygon):
-            return self.mirrorPolygon(self.shape)
-        elif isinstance(self.shape, Arc):
-            return self.mirrorArc(self.shape)
 
 polyOpacity = 100
 # Polygon(200,370,178,352,162,336,82,215,77,203,68,169,65,149,64,123,65,96,67,87,70,84,82,76,94,71,110,66,121,65,    306,71,307,72,318,76,330,84,333,87,335,96,336,123,335,149,332,169,323,203,318,215,238,336,222,352,     fill=gradient(rgb(133,179,208), 'white', rgb(133,179,208),start='left'),border='black',opacity=polyOpacity)
 # Polygon(200,336,15,336,8,233,7,213,10,196,14,176,32,188,49,198,67,207,84,214,98,218,110,221,123,224,137,226,150,228,161,229,175,230,183,230,193,230,201,230,200,230,        199,230,207,230,217,230,225,230,239,229,250,228,263,226,277,224,290,221,302,218,316,214,333,207,351,198,368,188,386,176,390,196,393,213,392,233,385,336,fill=gradient('white', rgb(133,179,208)),border='black',opacity=polyOpacity)
 
-draw_bezier([(264, 217), (262, 240), (228, 245), (212, 214)], 15)
+Bezier([(264, 217), (262, 240), (228, 245), (212, 214)], 15)
 
-draw_bezier([(211, 214), (159, 145), (82, 220), (176, 157)], 15)
+Bezier([(211, 214), (159, 145), (82, 220), (176, 157)], 15)
 
 # jag.toFront()
 
 app.selectedStartPoint = Circle(app.p0[0], app.p0[1], 5, fill='crimson', border='black', borderWidth=1, visible=False)
-app.bezier.toFront()
-app.bezier.visible = False
-app.bezierLine.toFront()
-# app.bezierLine.visible = False
+app.bezierLine.group.toFront()
 
 app.p0Label.toFront()
 app.p1Label.toFront()
@@ -230,6 +124,7 @@ def onKeyPress(key):
         else:
             app.selectedStart = 1
         changeStartPoint()
+        return
 
     if app.selectingNewStart and key == 'left':
         if app.selectedStart == 1:
@@ -237,6 +132,7 @@ def onKeyPress(key):
         else:
             app.selectedStart = 1
         changeStartPoint()
+        return
 
     if app.selectingNewStart and key == 'right':
         if app.selectedStart == 1:
@@ -244,15 +140,16 @@ def onKeyPress(key):
         else:
             app.selectedStart = 1
         changeStartPoint() 
+        return
 
     if app.selectingNewStart and key == 'enter':
         app.selectingNewStart = False
         app.selectedStartPoint.visible = False
-        app.bezierLines.add(app.bezierLine)
-        app.bezierLine = create_bezier_group([app.p0,app.p1,app.p2,app.p3],15)
+        app.bezierLines.append(app.bezierLine)
+        app.bezierLine = Bezier([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
         app.lockedPoint = app.selectedStart
-        app.bezierLines.toFront()
-        app.bezierLine.toFront()
+        app.bezierLine.group.toFront()
+        return
 
 
     if key == '0':
@@ -272,14 +169,12 @@ def onKeyPress(key):
         app.selectedPoint.value = '4'
     elif key == 'up' or key == '=':
         app.bezierPolygonalPoints += 1
-        # draw_bezier_sketch([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
-        update_bezier_group(app.bezierLine, [app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
+        app.bezierLine.update([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
         app.polypoints.value = str(app.bezierPolygonalPoints)
     elif key == 'down' or key == '-':
         if app.bezierPolygonalPoints > 2:
             app.bezierPolygonalPoints -= 1
-            # draw_bezier_sketch([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
-            update_bezier_group(app.bezierLine, [app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
+            app.bezierLine.update([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
             app.polypoints.value = str(app.bezierPolygonalPoints)
     elif key == 'enter':
         print(f"draw_bezier({[tuple(app.p0), tuple(app.p1), tuple(app.p2), tuple(app.p3)]}, {app.bezierPolygonalPoints})")
@@ -306,8 +201,7 @@ def onMouseDrag(x, y):
         app.p3Label.centerX = x
         app.p3Label.centerY = y
         app.p3Label.toFront()
-    # draw_bezier_sketch([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
-    update_bezier_group(app.bezierLine, [app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
+    app.bezierLine.update([app.p0, app.p1, app.p2, app.p3], app.bezierPolygonalPoints)
 
 
 
